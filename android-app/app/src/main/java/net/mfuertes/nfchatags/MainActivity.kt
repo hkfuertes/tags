2609dc819adc83ff5e25a3baacea15164a654db2
Miguel Fuertes <hkfuertes@gmail.com>
2022-02-27 02:31:10 +0100

    working poc android nfc

--- android-app/app/src/main/java/net/mfuertes/nfchatags/MainActivity.kt
+++ android-app/app/src/main/java/net/mfuertes/nfchatags/MainActivity.kt
@@ -2,87 +2,71 @@ package net.mfuertes.nfchatags
 
 import android.app.PendingIntent
 import android.content.Intent
-import android.content.IntentFilter
 import android.nfc.NfcAdapter
-import android.nfc.tech.NfcF
+import android.nfc.Tag
 import android.os.Bundle
-import android.provider.Settings
-import android.util.Log
+import android.os.Parcelable
 import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-    private var intentFiltersArray: Array<IntentFilter>? = null
-    private val techListsArray = arrayOf(arrayOf(NfcF::class.java.name))
-    private val nfcAdapter: NfcAdapter? by lazy {
-        NfcAdapter.getDefaultAdapter(this)
-    }
-    private var pendingIntent: PendingIntent? = null
-
     private lateinit var _tagIdText: TextView
+    private var nfcAdapter: NfcAdapter? = null
+    var pendingIntent: PendingIntent? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         _tagIdText = findViewById<TextView>(R.id.tagid)
-        try {
-            //nfc process start
-            pendingIntent = PendingIntent.getActivity(
-                this, 0, Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0
-            )
-            val ndef = IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED)
-            try {
-                ndef.addDataType("text/plain")
-            } catch (e: IntentFilter.MalformedMimeTypeException) {
-                throw RuntimeException("fail", e)
-            }
-            intentFiltersArray = arrayOf(ndef)
-            if (nfcAdapter == null) {
-                val builder = AlertDialog.Builder(this@MainActivity)
-                builder.setMessage("This device doesn't support NFC.")
-                builder.setPositiveButton("Cancel", null)
-                val myDialog = builder.create()
-                myDialog.setCanceledOnTouchOutside(false)
-                myDialog.show()
 
-
-            } else if (!nfcAdapter!!.isEnabled) {
-                val builder = AlertDialog.Builder(this@MainActivity)
-                builder.setTitle("NFC Disabled")
-                builder.setMessage("Plesae Enable NFC")
-
-                builder.setPositiveButton("Settings") { _, _ -> startActivity(Intent(Settings.ACTION_NFC_SETTINGS)) }
-                builder.setNegativeButton("Cancel", null)
-                val myDialog = builder.create()
-                myDialog.setCanceledOnTouchOutside(false)
-                myDialog.show()
-            }
-        } catch (ex: Exception) {
-            Toast.makeText(applicationContext, ex.message, Toast.LENGTH_SHORT).show()
-        }
+        this.nfcAdapter = NfcAdapter.getDefaultAdapter(this)?.let { it }
+        pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            Intent(this, this.javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+            0
+        )
     }
 
-    override fun onNewIntent(intent: Intent?) {
 
-        val tagId = intent?.getByteArrayExtra(NfcAdapter.EXTRA_ID)
-        //Toast.makeText(this,tagId.toString(),Toast.LENGTH_LONG).show()
-        Log.d("NFCHATAGSID", tagId.toString())
-        if (_tagIdText != null && tagId != null) {
-            _tagIdText.text = tagId.toString()
-        }
-        super.onNewIntent(intent)
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        if (intent != null) {
+            resolveIntent(intent)
+        };
     }
 
     override fun onResume() {
         super.onResume()
-        nfcAdapter?.enableForegroundDispatch(
-            this,
-            pendingIntent,
-            intentFiltersArray,
-            techListsArray
-        )
+        assert(nfcAdapter != null)
+        nfcAdapter!!.enableForegroundDispatch(this, pendingIntent, null, null)
+
+        //We recover the field from the intent
+        val id = this.intent.getByteArrayExtra("tag_id");
+        if (id != null) {
+            _tagIdText.text = id.toHex()
+        }
+
+    }
+
+    fun ByteArray.toHex(): String = joinToString(separator = "") { eachByte -> "%02x".format(eachByte) }
+
+
+    override fun onPause() {
+        super.onPause()
+        //Onpause stop listening
+        if (nfcAdapter != null) {
+            nfcAdapter!!.disableForegroundDispatch(this)
+        }
+    }
+
+    private fun resolveIntent(intent: Intent) {
+        val action = intent.action
+        if (NfcAdapter.ACTION_TAG_DISCOVERED == action || NfcAdapter.ACTION_TECH_DISCOVERED == action || NfcAdapter.ACTION_NDEF_DISCOVERED == action) {
+            val tag = (intent.getParcelableExtra<Parcelable>(NfcAdapter.EXTRA_TAG) as Tag?)!!
+            intent.putExtra("tag_id", tag.id)
+        }
     }
 }
