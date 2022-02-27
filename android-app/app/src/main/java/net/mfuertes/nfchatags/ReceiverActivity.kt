e8e0db3c1abb08d3e03cb696ac3daeea7a767e72
Miguel Fuertes <hkfuertes@gmail.com>
2022-02-27 13:35:33 +0100

    Android POC working

---
+++ android-app/app/src/main/java/net/mfuertes/nfchatags/ReceiverActivity.kt
@@ -0,0 +1,11 @@
+package net.mfuertes.nfchatags
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReceiverActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receiver)
+    }
+}
